13803009944b0dcc600adabc1f08aadb4eef30ec
ABUSAIDSHABIB <abusaidshabib712@gmail.com>
2022-11-12 03:43:45 +0600

    3:43 AM

--- src/Pages/Home/Hero/Hero.js
+++ src/Pages/Home/Hero/Hero.js
@@ -4,19 +4,22 @@ import './Hero.css';
 
 const Hero = () => {
     return (
-        <div className='hero-section md:text-left text-center px-16 py-20 text-white'>
-            <h1 className='md:text-5xl text-4xl font-bold leading-tight mb-16'>DESIGN YOUR WEBSITE WITH <br />
+        <div className='hero-section md:text-left 2xl:pl-48 min-h-screen text-center px-16 2xl:py-28 py-20 text-white'>
+            <h1 className='md:text-5xl text-4xl 2xl:text-7xl font-bold 2xl:leading-snug leading-tight mb-16'>DESIGN YOUR WEBSITE WITH <br />
                 HOSTING AND USE BEST MARKETING<br />
                 STRATEGY
             </h1>
-            <div className='grid md:grid-cols-2 grid-cols-1'>
+            <div className='w-1/5 bg-white h-2'>
+                
+            </div>
+            <div className='grid md:grid-cols-2 grid-cols-1 2xl:mt-10'>
                 <div>
-                    <p className='pr-16 mb-12'>
+                    <p className='pr-16 mb-12 2xl:text-3xl 2xl:leading-normal'>
                         We will design your website with the best options and help to complete our full services.
                     </p>
-                    <div className='flex items-center justify-center md:justify-start'>
+                    <div className='flex items-center justify-center 2xl:text-xl md:justify-start 2xl:mt-20'>
                         <Link className='mr-10'>
-                            <button className=" bg-blue-500 font-semibold hover:bg-white hover:text-blue-500 py-2 px-4 rounded">
+                            <button className="portfoliobtnBg hover:bg-transparent font-semibold hover:bg-white hover:text-blue-500 2xl:py-3 2xl:px-6 py-2 px-4 rounded">
                                 Portfolio
                             </button>
                         </Link>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -16,13 +16,13 @@ const Header = () => {
                         <img className='w-32 md:w-36 lg:w-40 xl:w-44 2xl:w-56' src={logo} alt="" />
                     </div>
                     <div className='text-right'>
-                        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                            Button
+                        <button className="bg-transparent 2xl:py-3 2xl:px-6 2xl:text-xl contactbtn font-semibold hover:text-white py-2 px-4 btnBorder rounded">
+                            Contact Us
                         </button>
                     </div>
                 </div>
             </div>
-            <div className='w-20'>
+            <div className='w-20 fixed left-0'>
                 <img className='w-10' src={home} alt=""/>
                 <br/>
                 <img className='w-10' src={service} alt=""/>
